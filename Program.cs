00bc93c8134c39f8a68d7fcccff634ef9a8c96b5
Rityo Salindra <nuruddin859@gmail.com>
2022-03-19 14:04:43 +0700

    menambahkan class Penjumlahan dengan method jumlah TigaAngka

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302204014
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hasil Penjumlahan Tiga Angka : " + Penjumlahan.JumlahTigaAngka<float>(20, 40, 14));
+        }
+    }
+
+    public class Penjumlahan
+    {
+        public static T JumlahTigaAngka<T>(T input1, T input2 , T input3 )
+        {
+            dynamic x = input1;
+            dynamic y = input2;
+            dynamic z = input3;
+
+            return x + y + z;
+        }
+    }
+
+    
+}
